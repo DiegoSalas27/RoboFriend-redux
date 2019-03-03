7945166a6b6346847d5b9e18f38882f12c00d0e4
diegosalas27 <dominicsc2hs@gmail.com>
2019-03-03 10:37:10 -0500

    Updates

---
+++ precache-manifest.db7262ffd882a80223bc2ecff137f0b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6779c871e189d49817578ce64323508",
+    "url": "/RoboFriend-redux/static/media/pokemon-font.d6779c87.woff"
+  },
+  {
+    "revision": "5182945b6872484d2d7d",
+    "url": "/RoboFriend-redux/static/js/runtime~main.5182945b.js"
+  },
+  {
+    "revision": "bfcef1fba1d3d78cb5ee",
+    "url": "/RoboFriend-redux/static/js/main.bfcef1fb.chunk.js"
+  },
+  {
+    "revision": "4be0038822ac38e387de",
+    "url": "/RoboFriend-redux/static/js/2.4be00388.chunk.js"
+  },
+  {
+    "revision": "bfcef1fba1d3d78cb5ee",
+    "url": "/RoboFriend-redux/static/css/main.1a2a2baa.chunk.css"
+  },
+  {
+    "revision": "4be0038822ac38e387de",
+    "url": "/RoboFriend-redux/static/css/2.2353cac4.chunk.css"
+  },
+  {
+    "revision": "bd0b62c96c46bb92fe8d1c8c872ef5db",
+    "url": "/RoboFriend-redux/index.html"
+  }
+];
